580b797209ac9acada312ce6b549fc401e27029a
Alex <zeroalexchen@outlook.com>
2020-06-12 01:11:56 +0800

    [beta1:加入蒙哥马利表达式预计算，模乘状态机]

--- rtl/moutmul.v
+++ rtl/moutmul.v
@@ -1,6 +1,50 @@
-module moutmul(x,y,z)
-    input   [2047,0] x;
-    input   [2047,0] y;
-    reg     [2047,0] i;
-    reg     [2047,0] temp1;
-    output  [2047,0] z;
+module moutmul(x,y,n,clk,result);
+    input   [2047:0]    x;
+    input   [2047:0]    y;
+    input   [2047:0]    n;
+    input   clk;
+
+    output  reg [2047:0]    result;
+
+    reg     [2047:0]    i;    
+    reg     [2047:0]    temp1;
+    reg     [2047:0]    temp2;
+    reg     [1:0]       status;
+
+    wire    [2047:0]    temp3;
+    wire    [2047:0]    temp4;
+
+    assign temp3 = ( x * 2**11 ) % n;
+    assign temp4 = ( y * 2**11 ) % n;
+
+    parameter [1:0] start = 2'b00,mul=2'b01, done=2'b10;
+
+always@(posedge clk) 
+
+    case (status)
+        start:
+            begin
+                i = 0;
+                status = mul;
+            end           
+        mul:
+            begin
+                result = (result + temp4[i]*temp3 + (result[0]^(temp4[i] && temp3[0])) * n) << 1;
+					 
+                if(!i)
+                begin
+                    i = i + 1;      
+                    status = mul ;
+                end
+                else
+                    status = done;
+			end
+					
+        done:
+            status = done;
+        default: 
+            status = start;
+    endcase 
+
+
+endmodule
